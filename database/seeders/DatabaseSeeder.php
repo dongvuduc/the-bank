057e165702677f8eefbe61822255f91247611a6f
vuduc <vuduc@miennam24h.vn>
2021-04-07 20:01:05 +0700

    Fix Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,38 +29,33 @@ class DatabaseSeeder extends Seeder
 
         DB::table('oauth_clients')->insert([
             'id' => '931e54e1-8741-42c4-8dad-b6ff7e2f5199',
-            'user_id' => '',
             'name' => 'Laravel Personal Access Client',
             'secret' => 'sQYT8Loq3dSeSGZcYBW3spCBfsgjS0xHuur70QfY',
-            'provider' => '',
             'redirect' => 'http://localhost',
-            'personal_access_client' => '1',
-            'password_client' => '0',
-            'revoked' => '0'
+            'personal_access_client' => 1,
+            'password_client' => 0,
+            'revoked' => 0
         ]);
 
         DB::table('oauth_clients')->insert([
             'id' => '931e54e1-8e2c-4d68-9acb-876caece37bd',
-            'user_id' => '',
             'name' => 'Laravel Password Grant Client',
             'secret' => '3SiCAuMY1eVtrqduFm2n0l9hS4LtTbiPw0aDmdbD',
             'provider' => 'users',
             'redirect' => 'http://localhost',
-            'personal_access_client' => '0',
-            'password_client' => '1',
-            'revoked' => '0'
+            'personal_access_client' => 0,
+            'password_client' => 1,
+            'revoked' => 0
         ]);
 
         DB::table('oauth_clients')->insert([
             'id' => '931f0006-4bbf-4773-bd9a-9584c8c62548',
-            'user_id' => '',
             'name' => 'The Office',
             'secret' => 'XUaDLBSBsjMt5NYmCNjp4DGVcSt4rfRLEx5gTRkq',
-            'provider' => '',
             'redirect' => 'http://localhost:8080/callback',
-            'personal_access_client' => '0',
-            'password_client' => '0',
-            'revoked' => '0'
+            'personal_access_client' => 0,
+            'password_client' => 0,
+            'revoked' => 0
         ]);
 
         DB::table('oauth_personal_access_clients')->insert([
